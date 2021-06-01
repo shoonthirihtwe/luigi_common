cc57a0c04c1c2a5ed876913bccb23e0ae9295fb2
ParkSunghoon <s.paku@allofthem.co.jp>
2021-06-01 10:33:19 +0900

    プロジェクト：Application共通化

---
+++ src/main/java/jp/co/ichain/luigi2/Luigi2Application.java
@@ -0,0 +1,15 @@
+package jp.co.ichain.luigi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("common/application-common.properties")
+public class Luigi2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Luigi2Application.class, args);
+  }
+
+}
