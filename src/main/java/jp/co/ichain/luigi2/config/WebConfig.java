ab36ee9c1ea6c9ea81a858a586c82af175cce42c
ParkSunghoon <s.paku@allofthem.co.jp>
2021-06-01 10:29:08 +0900

    プロジェクト：DB設定Commonに移行

---
+++ src/main/java/jp/co/ichain/luigi2/config/WebConfig.java
@@ -0,0 +1,17 @@
+package jp.co.ichain.luigi2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * プロジェクトの関連設定を定義
+ * 
+ * @author : [AOT] s.paku
+ * @createdAt : 2021-06-01
+ * @updatedAt : 2021-06-01
+ */
+@Configuration
+@PropertySource("common/application-common.properties")
+public class WebConfig {
+
+}
