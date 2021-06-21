478d6ace469a68969acca6aa9bfb44ed9668d084
hoangquyetthangvjp <66122600+hoangquyetthangvjp@users.noreply.github.com>
2021-06-21 18:02:59 +0900

    機能追加 :機能の追加でコード修正
    BD-002

---
+++ src/main/java/jp/co/ichain/luigi2/vo/DepositDetailsVo.java
@@ -0,0 +1,152 @@
+package jp.co.ichain.luigi2.vo;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import jp.co.ichain.luigi2.config.JsonDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/***
+ * DepositDetailsVo
+ *
+ * @author : [VJP] タン
+ * @createdAt : 2021-06-18
+ * @updatedAt : 2021-06-18
+ */
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositDetailsVo extends ObjectVo {
+  /**
+   * ID
+   */
+  Integer id;
+  
+  /**
+   * テナントID
+   */
+  Integer tenantId;
+  
+  /**
+   * 入力日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date entryDate;
+  
+  /**
+   * バッチナンバー
+   */
+  Integer batchNo;
+  
+  /**
+   * 明細番号
+   */
+  Integer cashDetailNo;
+  
+  /**
+   * 契約番号
+   */
+  String contractNo;
+  
+  /**
+   * 申込番号
+   */
+  String applicationNo;
+  
+  /**
+   * 充当月
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date dueDate;
+  
+  /**
+   * 合計保険料金額
+   */
+  Integer totalPremiumAmount;
+  
+  /**
+   * 入金金額
+   */
+  Integer depositAmount;
+  
+  /**
+   * 振込手数料
+   */
+  Integer commissionWithheld;
+  
+  /**
+   * 振込手数料 消費税
+   */
+  Integer compensationTax;
+  
+  /**
+   * 消し込み日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date clearingDate;
+  
+  /**
+   * サスペンス日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date suspenceDate;
+  
+  /**
+   * 削除日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date deleteDate;
+  
+  /**
+   * マッチング日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date cashMatchingDate;
+  
+  /**
+   * 明細のステータス
+   */
+  String cashDetailStatus;
+  
+  /**
+   * 引き去り結果コード
+   */
+  String paymentResultCode;
+  
+  /**
+   * 備考
+   */
+  String comment;
+  
+  /**
+   * 保険料充当日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date premiumDueDate;
+  
+  /**
+   * 保険料　連番
+   */
+  Integer premiumSequenceNo; 
+  
+  /**
+   * ロック用
+   */
+  Integer updateCount;
+  
+  /**
+   * 作成者
+   */
+  String createdBy;
+  
+  /**
+   * 最終更新者
+   */
+  String updatedBy;
+}
---
+++ src/main/java/jp/co/ichain/luigi2/vo/DepositHeadersVo.java
@@ -0,0 +1,107 @@
+package jp.co.ichain.luigi2.vo;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import jp.co.ichain.luigi2.config.JsonDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/***
+ * DepositHeadersVo
+ *
+ * @author : [VJP] タン
+ * @createdAt : 2021-06-18
+ * @updatedAt : 2021-06-18
+ */
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositHeadersVo extends ObjectVo {
+  /**
+   * ID
+   */
+  Integer id;
+  
+  /**
+   * テナントID
+   */
+  Integer tenantId;
+  
+  /**
+   * 入力日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date entryDate;
+  
+  /**
+   * バッチナンバー
+   */
+  Integer batchNo;
+  
+  /**
+   * 払込方法コード
+   */
+  String paymentMethodCode;
+  
+  /**
+   * 入金日
+   */
+  @JsonSerialize(using = JsonDateSerializer.class)
+  Date depositDate;
+  
+  /**
+   * 入金金額
+   */
+  String  receivedAmount;
+  
+  /**
+   * バッチ合計金額
+   */
+  String batchTotalAmount;
+  
+  /**
+   * ステータス
+   */
+  String batchStatus;
+  
+  /**
+   * 備考
+   */
+  String comment;
+  
+  /**
+   * 処理ユーザーID
+   */
+  String usereId;
+  
+  /**
+   * 収集ルート
+   */
+  String collectionRoute;
+  
+  /**
+   * 団体コード
+   */
+  String groupCode;
+  
+  /**
+   * ロック用
+   */
+  Integer updateCount;
+  
+  /**
+   * 作成者
+   */
+  String createdBy;
+  
+  /**
+   * 最終更新者
+   */
+  String updatedBy;
+}
