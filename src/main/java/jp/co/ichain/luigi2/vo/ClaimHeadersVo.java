25a1d98c4f76d86b89bfe055820807c5734763f2
hoangquyetthangvjp <66122600+hoangquyetthangvjp@users.noreply.github.com>
2021-06-24 16:07:46 +0900

    機能追加 :機能の追加でコード修正
    Joinについて対応

--- src/main/java/jp/co/ichain/luigi2/vo/ClaimHeadersVo.java
+++ src/main/java/jp/co/ichain/luigi2/vo/ClaimHeadersVo.java
@@ -423,4 +423,19 @@ public class ClaimHeadersVo extends ObjectVo {
    */
   @JsonIgnore
   String deletedBy;
+  
+  /**
+   * 商品名
+   */
+  String salesPlanName;
+  
+  /**
+   * 請求者氏名
+   */
+  String claimantNameKnj;
+  
+  /**
+   * 請求者氏名（カナ）
+   */
+  String claimantNameKana;
 }
