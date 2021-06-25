fa260cdf6798be9b10a7c9d73b62bdca9e463dc3
hoangnhvjp <nguyen.huy.hoang@vjpco.com>
2021-06-25 09:15:56 +0900

    機能追加 :機能の追加でコード修正(BD008-保険料データ作成)

---
+++ src/main/java/jp/co/ichain/luigi2/vo/ContractPremiumHeader.java
@@ -0,0 +1,55 @@
+package jp.co.ichain.luigi2.vo;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/***
+ * ContractPremiumHeader
+ *
+ * @author : [VJP] n.h.hoang
+ * @createdAt : 2021-06-24
+ * @updatedAt : 2021-06-24
+ */
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContractPremiumHeader extends ObjectVo {
+
+  /**
+   * tenantId
+   */
+  Integer tenantId;
+  
+  /**
+   * 証券番号
+   */
+  String contractNo;
+  
+  /**
+   * 証券番号枝番
+   */
+  String contractBranchNo;
+  
+  /**
+   * 現契約日
+   */
+  Date issueDate;
+  
+  /**
+   * 合計保険料
+   */
+  Integer totalPremium;
+  
+  /**
+   * 保険料払込回数
+   */
+  String frequency;
+  
+}
