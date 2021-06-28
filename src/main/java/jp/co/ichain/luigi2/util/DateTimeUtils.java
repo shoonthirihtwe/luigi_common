0987a15c15810349368d5caa64ea674b125f0801
hoangnhvjp <nguyen.huy.hoang@vjpco.com>
2021-06-28 13:16:54 +0900

    Revert "機能追加 :機能の追加でコード修正"
    This reverts commit 460b94016d150a30b827125f34afa77358c73e23.

--- src/main/java/jp/co/ichain/luigi2/util/DateTimeUtils.java
+++ src/main/java/jp/co/ichain/luigi2/util/DateTimeUtils.java
@@ -3,7 +3,6 @@ package jp.co.ichain.luigi2.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -28,10 +27,6 @@ public class DateTimeUtils {
     return date == null ? null : formatter.format(date).substring(0, 6);
   }
 
-  public static String convertDateToDay(Date date) {
-    return date == null ? null : formatter.format(date).substring(6, 8);
-  }
-
   /**
    * add month to Date(Date with format yyyyMM)
    * 
@@ -40,21 +35,11 @@ public class DateTimeUtils {
    * @return
    * @throws ParseException
    */
-  public static String addMonthToYearMonth(String dateString, int addMonth) {
+  public static String addDateToYearMonth(String dateString, int addMonth) {
     dateString += "01";
     LocalDate localDate = toDate(dateString);
-    localDate = localDate.plusMonths(addMonth);
+    localDate = localDate.plusMonths(3);
     return toString(localDate).substring(0, 6);
-  }
-
-  public static Date addDayToYearMonth(String yyyyMm, Integer dd) {
-    yyyyMm += "01";
-    LocalDate currentDate = toDate(yyyyMm);
-    LocalDate nextDate = currentDate.plusDays(dd - 1);
-    if (currentDate.getMonthValue() != nextDate.getMonthValue()) {
-      return convertLocalDateToDate(nextDate.plusDays(1 - nextDate.getDayOfMonth()));
-    }
-    return convertLocalDateToDate(nextDate);
   }
 
   /**
@@ -66,14 +51,6 @@ public class DateTimeUtils {
   public static LocalDate toDate(String date) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
     return LocalDate.parse(date, formatter);
-  }
-
-  public static Date convertLocalDateToDate(LocalDate localdate) {
-    // default time zone
-    ZoneId defaultZoneId = ZoneId.systemDefault();
-
-    // local date + atStartOfDay() + default time zone + toInstant() = Date
-    return Date.from(localdate.atStartOfDay(defaultZoneId).toInstant());
   }
 
   /**
