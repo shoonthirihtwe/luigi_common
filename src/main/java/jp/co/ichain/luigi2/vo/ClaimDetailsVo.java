c69209c7f7cc725f2e8868a73e32adff6fa27ba7
ParkSunghoon <s.paku@allofthem.co.jp>
2021-07-14 10:54:52 +0900

    チェックスタイル適用

--- src/main/java/jp/co/ichain/luigi2/vo/ClaimDetailsVo.java
+++ src/main/java/jp/co/ichain/luigi2/vo/ClaimDetailsVo.java
@@ -4,8 +4,6 @@ import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jp.co.ichain.luigi2.config.web.JsonDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -50,7 +48,7 @@ public class ClaimDetailsVo extends ObjectVo {
    * 証券番号枝番
    */
   String contractBranchNo;
-  
+
   /**
    * 連番
    */
@@ -60,137 +58,137 @@ public class ClaimDetailsVo extends ObjectVo {
    * 有効/無効フラグ
    */
   String activeInactive;
-  
+
   /**
    * 主契約／特約区分
    */
   String baseRider;
-  
+
   /**
    * 商品タイプ
    */
   String productType;
-  
+
   /**
    * 保険種別コード
    */
   String policyCode;
-  
+
   /**
    * 商品バージョン
    */
   String version;
-  
+
   /**
    * 保険プラン
    */
   String salesPlanCode;
-  
+
   /**
    * 販売プラン種別コード
    */
   String salesPlanTypeCode;
-  
+
   /**
    * 保険金タイプ
    */
   String benefitCode;
-  
+
   /**
    * 保険金名称
    */
   String benefitText;
-  
+
   /**
    * 支払タイプ
    */
   String paymentCode;
-  
+
   /**
    * 支払タイプ名称
    */
   String paymentText;
-  
+
   /**
    * 給付日額
    */
   Integer dailyBenefitAmount;
-  
+
   /**
    * 請求内容
    */
   String claimDetailInfo;
-  
+
   /**
    * 請求日数
    */
   Integer claimDays;
-  
+
   /**
    * 請求額
    */
   Integer claimAmount;
-  
+
   /**
    * 保険対象外金額
    */
   Integer noncoverageAmount;
-  
+
   /**
    * 保険対象金額
    */
   Integer coverageAmount;
-  
+
   /**
    * 保障割合
    */
   Integer compenseRate;
-  
+
   /**
    * 免責額・自己負担額
    */
   Integer disclaimAmount;
-  
+
   /**
    * 都度支払限度額
    */
   Integer eachtimeLimitAmount;
-  
+
   /**
    * 通算支払限度額
    */
   Integer totalLimitAmount;
-  
+
   /**
    * 支払対象額
    */
   Integer benefitAmount;
-  
+
   /**
    * 通算超過額
    */
   Integer overAmount;
-  
+
   /**
    * 他社支払額
    */
   Integer othersPaidAmount;
-  
+
   /**
    * 支払決定額
    */
   Integer amountToPay;
-  
+
   /**
    * 他保険
    */
   String otherInsuerer;
-  
+
   /**
    * その他商品調整額
    */
   Integer customProductAdjustment;
-  
+
   /**
    * その他商品調整額題目
    */
