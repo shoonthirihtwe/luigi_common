90175fb78aad7e2088f29e08777727ceff88e87e
ParkSunghoon <s.paku@allofthem.co.jp>
2021-08-02 14:25:50 +0900

    機能追加：保全住所変更顧客取得追加

---
+++ src/main/java/jp/co/ichain/luigi2/vo/MaintenanceRequestsTransferVo.java
@@ -0,0 +1,43 @@
+package jp.co.ichain.luigi2.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 保全名義変更顧客情報
+ * 
+ * @author : [AOT] s.paku
+ * @createdAt : 2021-07-30
+ * @updatedAt : 2021-07-30
+ */
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class MaintenanceRequestsTransferVo extends ObjectVo {
+
+  /**
+   * 契約者顧客情報
+   */
+  MaintenanceRequestsCustomersVo contractorCustomer;
+
+  /**
+   * 契約者後見人顧客情報
+   */
+  MaintenanceRequestsCustomersVo contractorGuardianCustomer;
+
+  /**
+   * 被保険者顧客情報
+   */
+  MaintenanceRequestsCustomersVo insuredCustomer;
+
+  /**
+   * 被保険者後見人顧客情報
+   */
+  MaintenanceRequestsCustomersVo insuredGuardianCustomer;
+}
