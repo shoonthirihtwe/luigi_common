cdfa96ca8132dcc3466de7e7318d5dd0aab8bd7d
g.kim <g.kim@allofthem.co.jp>
2021-12-02 14:38:49 +0900

    機能追加：コードリソース更新

--- src/main/java/jp/co/ichain/luigi2/resources/code/Luigi2CodeClaimHeaders.java
+++ src/main/java/jp/co/ichain/luigi2/resources/code/Luigi2CodeClaimHeaders.java
@@ -12,10 +12,10 @@ public class Luigi2CodeClaimHeaders {
   /**
    * 申請ステータス
    *
-   * 2:１次査定中
-   * 3:２次査定中
-   * 4:差戻中
-   * 5:査定完了
+   * 2:１次査定中 
+   * 3:２次査定中 
+   * 4:申請ステータス：5 かつ査定ステータス：3 
+   * 5:申請ステータス：5 かつ査定ステータス：2 or 5(無査定支払) 
    * 6:支払処理済
    * 
    * @author : [VJP] タン
@@ -48,12 +48,42 @@ public class Luigi2CodeClaimHeaders {
   }
 
   /**
+   * Claim 検索ステータス 
+   * 0:受付済 
+   * 1:１次査定中 
+   * 2:２次査定中 
+   * 3:2次査定済
+   * 4:支払承認済
+   * 5:支払処理済
+   * 
+   * @author : [AOT] g.kim
+   * @createdAt : 2021-12-01
+   * @updatedAt : 2021-12-01
+   */
+  public enum SearchClaimStatus {
+    RECEPTION_COMPLETED("0"), FIRST_ASSESSMENT("1"), SECOND_ASSESSMENT(
+        "2"), SECOND_ASSESSMENT_COMPLETED("3"), PAYMENT_APPROVAL_COMPLETED("4"), PAYMENT_PROCESSED("5");
+
+    String val;
+
+    SearchClaimStatus(String val) {
+      this.val = val;
+    }
+
+    @Override
+    public String toString() {
+      return this.val;
+    }
+  }
+
+
+  /**
    * 査定ステータス
    *
-   * 1:査定前
-   * 2:承諾
-   * 3:不承諾
-   * 4:不備
+   * 1:査定前 
+   * 2:承諾 
+   * 3:不承諾 
+   * 4:不備 
    * 5:無査定支払
    * 
    * @author : [VJP] タン
