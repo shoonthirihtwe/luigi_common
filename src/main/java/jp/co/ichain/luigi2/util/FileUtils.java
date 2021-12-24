8b74cbdcc1e7967a5163e3aa2d9e976f01e9c2e5
g.kim <g.kim@allofthem.co.jp>
2021-12-24 11:48:28 +0900

    その他：Indent修正

--- src/main/java/jp/co/ichain/luigi2/util/FileUtils.java
+++ src/main/java/jp/co/ichain/luigi2/util/FileUtils.java
@@ -18,36 +18,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class FileUtils {
 
-	/**
-	 * read textFile
-	 * 
-	 * @author : [AOT] g.kim
-	 * @createdAt : 2021-07-13
-	 * @updatedAt : 2021-07-13
-	 * @param src
-	 * @return
-	 * @throws SQLException
-	 * @throws ScriptException
-	 */
-	public static String readTextFile(InputStream inputStream, boolean removeBom) throws IOException {
+  /**
+   * read textFile
+   * 
+   * @author : [AOT] g.kim
+   * @createdAt : 2021-07-13
+   * @updatedAt : 2021-07-13
+   * @param src
+   * @return
+   * @throws SQLException
+   * @throws ScriptException
+   */
+  public static String readTextFile(InputStream inputStream, boolean removeBom) throws IOException {
 
-		ByteArrayOutputStream expectedFile = new ByteArrayOutputStream();
-		inputStream.transferTo(expectedFile);
+    ByteArrayOutputStream expectedFile = new ByteArrayOutputStream();
+    inputStream.transferTo(expectedFile);
 
-		byte[] readedBytes = null;
-		byte[] data = expectedFile.toByteArray();
+    byte[] readedBytes = null;
+    byte[] data = expectedFile.toByteArray();
 
-		if ((data[0] & 0xFF) == 0xEF && (data[1] & 0xFF) == 0xBB && (data[2] & 0xFF) == 0xBF && removeBom) {
+    if ((data[0] & 0xFF) == 0xEF && (data[1] & 0xFF) == 0xBB && (data[2] & 0xFF) == 0xBF
+        && removeBom) {
 
-			int len = data.length - 3;
-			readedBytes = new byte[len];
-			System.arraycopy(data, 3, readedBytes, 0, len);
+      int len = data.length - 3;
+      readedBytes = new byte[len];
+      System.arraycopy(data, 3, readedBytes, 0, len);
 
-		} else {
-			readedBytes = data;
-		}
+    } else {
+      readedBytes = data;
+    }
 
-		return new String(readedBytes, "utf-8");
-	}
+    return new String(readedBytes, "utf-8");
+  }
 
 }
