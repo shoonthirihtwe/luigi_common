12f3c60ad1475c08a81f56f798e50d2d7f39a1b4
ParkSunghoon <s.paku@allofthem.co.jp>
2022-04-04 11:18:45 +0900

    機能修正：BatchServiceにGmoPaymentException追加

--- src/main/java/jp/co/ichain/luigi2/batch/BatchService.java
+++ src/main/java/jp/co/ichain/luigi2/batch/BatchService.java
@@ -7,6 +7,7 @@ import java.text.ParseException;
 import java.util.List;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import jp.co.ichain.luigi2.exception.GmoPaymentException;
 import jp.co.ichain.luigi2.vo.TenantsVo;
 
 public interface BatchService {
@@ -14,5 +15,5 @@ public interface BatchService {
   public void run(List<TenantsVo> tenantList)
       throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
       JsonMappingException, JsonProcessingException, ParseException,
-      UnsupportedEncodingException, IOException;
+      UnsupportedEncodingException, IOException, GmoPaymentException;
 }
