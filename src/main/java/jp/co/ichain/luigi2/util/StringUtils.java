7b450e77db4d3da385481050d1f0f979c4d4c218
ParkSunghoon <s.paku@allofthem.co.jp>
2022-08-30 14:00:08 +0900

    機能追加：カタカナ→ひらがな追加

--- src/main/java/jp/co/ichain/luigi2/util/StringUtils.java
+++ src/main/java/jp/co/ichain/luigi2/util/StringUtils.java
@@ -24,4 +24,45 @@ public class StringUtils {
     }
     return false;
   }
+
+  /**
+   * カタカナ→ひらがな
+   * 
+   * @author : [AOT] s.paku
+   * @createdAt : 2022/08/30
+   * @updatedAt : 2022/08/30
+   * @param str
+   * @return
+   */
+  public static String convertHiraToKata(String str) {
+    if (str == null)
+      return null;
+
+    StringBuffer buf = new StringBuffer();
+    for (int i = 0; i < str.length(); i++) {
+      char code = str.charAt(i);
+      if ((code >= 0x30a1) && (code <= 0x30f3)) {
+        buf.append((char) (code - 0x60));
+      } else {
+        buf.append(code);
+      }
+    }
+    return new String(buf);
+  }
+
+  /**
+   * ハイフン付き郵便番号作成
+   * 
+   * @author : [AOT] s.paku
+   * @createdAt : 2022/08/30
+   * @updatedAt : 2022/08/30
+   * @param str
+   * @return
+   */
+  public static String makeHyphenZipcode(String str) {
+    if (str == null || str.length() != 7)
+      return str;
+
+    return str.substring(0, 3) + "-" + str.substring(3, 7);
+  }
 }
